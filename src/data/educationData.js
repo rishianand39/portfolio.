5c027bb054e304906f20cc518b6824cc38d12477
Rishi Anand <rishi.rn818@gmail.com>
2022-11-04 10:49:43 +0530

    startyear and endyear of education modified

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -6,25 +6,25 @@ export const educationData = [
         startYear: ' Nov, 2021',
         endYear: ' July, 2022'
     },
-    {
-        id: 2,
-        institution: 'India Gandhi National Open University, New Delhi, India',
-        course: 'Bachelor of Computer Application',
-        startYear: '2021',
-        endYear: 'present'
-    },
+//     {
+//         id: 2,
+//         institution: 'India Gandhi National Open University, New Delhi, India',
+//         course: 'Bachelor of Computer Application',
+//         startYear: '2021',
+//         endYear: 'present'
+//     },
     {
         id: 3,
         institution: 'Paras Nath Kushwaha College, Acchua, Paliganj, Patna, Bihar(BSEB)',
-        course: 'Intermediate (science)',
-        startYear: '2014',
-        endYear: '2019'
+        course: '12th (science)',
+        startYear: '2019',
+        endYear: '2021'
     },
     {
         id: 4,
         institution: 'Bihar Public School, Paliganj, Patna, Bihar (CBSE)',
         course: 'Schooling',
-        startYear: '2019',
-        endYear: '2021'
+        startYear: '2014',
+        endYear: '2019'
     },
 ]
