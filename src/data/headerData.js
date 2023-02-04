04666f4d5c71b05e13dadad44a630b65bfce904e
rishianand39 <rishi.rn818@gmail.com>
2023-02-04 21:02:10 +0530

    profile picture changed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,7 @@ export const headerData = {
     desciption:`Aspiring full-stack developer with a specialization in MERN stack. Self-motivated and
     curious, with a keen interest in building user-centric products. Looking forward to
     working in a challenging work environment.`,
-    image:'https://firebasestorage.googleapis.com/v0/b/marriage-portal-f7ed4.appspot.com/o/rishi%20anand_profile_photo.png?alt=media&token=67b025f3-3447-4ce7-a7f5-e37a77b5759a' ,
+    image:"https://firebasestorage.googleapis.com/v0/b/marriage-portal-f7ed4.appspot.com/o/Rish%20Anand%20Professional%20picture.jpg?alt=media&token=d383195a-4a6b-4c92-9295-c8c130893228",
+    // image:'https://firebasestorage.googleapis.com/v0/b/marriage-portal-f7ed4.appspot.com/o/rishi%20anand_profile_photo.png?alt=media&token=67b025f3-3447-4ce7-a7f5-e37a77b5759a' ,
     resumePdf: "https://drive.google.com/file/d/1baBLotRB9dD2lkyaxVnBclsEN0-AhJET/view?usp=sharing"
 }
